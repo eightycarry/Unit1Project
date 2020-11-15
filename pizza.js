a737a3dc355cf1408016c2f4f823fcf3d1de3f22
eightycarry <calvin_lor@yahoo.com>
2020-11-15 03:51:35 -0600

    Pizza Order

--- pizza.js
+++ pizza.js
@@ -1,11 +1,51 @@
 $(document).ready(function () {
+    $("#tabs a").click(showTab);
 
-        // event handler(s)
-        $("#tabs a").click(showTab);
+    function showTab(event) {
+        event.preventDefault();
+        $(this).tab("show");
+        $("form").submit(placeOrder);
+    }
+    function placeOrder(event) {
+        //stop the flashing form
+        event.preventDefault();
 
-        // functions
-         function showTab(event) {
-            event.preventDefault();
-            $(this).tab("show");
-        }
-    });
+        //make a subtotal variable
+        var subtotal = 0;
+
+        //make a name variable
+        var name = $("#name").val();
+
+        //make an address variable
+        var address = $("#address").val();
+
+        //make a phone number variable
+        var phoneNumber = $("#phone").val();
+
+        //ask jQuery for selected pizza size
+        var selectedSize = $("input[name=size]:checked");
+
+        //get the data-price value for the selected size
+        selectedSize.each(function() {
+            subtotal += $(this).data("price");
+        })
+
+        //ask jQuery for selected pizza crust
+        var selectedCrust = $("input[name=crust]:checked");
+
+        //ask jQuery for all selected meats boxes
+        var selectedMeats = $("input[name=meats]:checked");
+
+        //ask jQuery for all selected veggie boxes
+        var selectedVeggies = $("input[name=veggies]:checked");
+
+        //output to review tab
+        $("#nameOutput").text(name);
+        $("#addressOutput").text(address);
+        $("#phoneOutput").text(phoneNumber);
+        $("#sizeOutput").text(selectedSize);
+        $("#crustOutput").text(selectedCrust);
+        $("#meatOutput").text(selectedMeats);
+        $("#veggieOutputOutput").text(selectedVeggies);
+    }
+});
