1a55b4562ea8a8e6d2e599d368628abad5c6a033
eightycarry <calvin_lor@yahoo.com>
2020-11-14 19:21:09 -0600

    Pizza Order

--- pizza.js
+++ pizza.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-});
+        // event handler(s)
+        $("#tabs a").click(showTab);
+
+        // functions
+         function showTab(event) {
+            event.preventDefault();
+            $(this).tab("show");
+        }
+    });
