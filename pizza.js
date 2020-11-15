094ab97d6dc969a896e873d6b03c17f1f8db79ad
eightycarry <calvin_lor@yahoo.com>
2020-11-15 08:07:42 -0600

    Pizza Order

--- pizza.js
+++ pizza.js
@@ -10,25 +10,20 @@ $(document).ready(function () {
         //stop the flashing form
         event.preventDefault();
 
-        //make a subtotal variable
-        var subtotal = 0;
-
-        //make a name variable
+        //make a name variable and retrieve the input
         var name = $("#name").val();
 
-        //make an address variable
+        //make an address variable and retrieve the input
         var address = $("#address").val();
 
-        //make a phone number variable
+        //make a phone number variable and retrieve the input
         var phoneNumber = $("#phone").val();
 
         //ask jQuery for selected pizza size
         var selectedSize = $("input[name=size]:checked").val();
 
         //get the data-price value for the selected size
-        /*selectedSize.each(function() {
-            subtotal += $(this).data("price");
-        })*/
+        var sizeTotal = parseFloat($("input[name=size]:checked").data("price"));
 
         //ask jQuery for selected pizza crust
         var selectedCrust = $("input[name=crust]:checked").val();
@@ -38,14 +33,30 @@ $(document).ready(function () {
         $("input[name=meats]:checked").each(function() {
             selectedMeats.push($(this).val());
         });
-        $('#meatOutput').val(selectedMeats);
+        //$('#meatOutput').val(selectedMeats);
+
+        //collect total price of selected meats
+        //check which meats were selected, then add $1.50 for each
+        var meatTotal = $("input[name=meats]:checked").length;
+        meatTotal *= 1.50;
 
         //ask jQuery for all selected veggie boxes
         var selectedVeggies = [];
         $("input[name=veggies]:checked").each(function() {
             selectedVeggies.push($(this).val());
         });
-        $('#veggieOutputOutput').val(selectedMeats);
+        //$('#veggieOutput').val(selectedMeats);
+
+        //collect total price of selected veggies
+        var veggieTotal = $("input[name=veggies]:checked").length;
+
+        //add the prices of everything to get subtotal
+        var subTotal;
+        subTotal = (sizeTotal + meatTotal + veggieTotal);
+
+        //add sales tax and delivery fee to subtotal
+        var taxTotal = subTotal * .051;
+        var grandTotal =  subTotal + taxTotal + 2.0;
 
         //output to review tab
         $("#nameOutput").text(name);
@@ -55,5 +66,9 @@ $(document).ready(function () {
         $("#crustOutput").text(selectedCrust);
         $("#meatOutput").text(selectedMeats);
         $("#veggieOutput").text(selectedVeggies);
+        $("#subtotalOutput").text(`Subtotal: $${parseFloat(subTotal).toFixed(2)}`);
+        $("#salesTaxOutput").text(`Sales Tax: $${parseFloat(taxTotal).toFixed(2)}`);
+        $("#deliveryFeeOutput").text(`Delivery Fee: $2.00`);
+        $("#totalOutput").text(`Your total is $${parseFloat(grandTotal).toFixed(2)}`);
     }
 });
